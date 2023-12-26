f9458de8992961b0d453d3295347db6b06f11f55
neleon10 <cvn45@hotmail.com>
2023-12-26 13:04:40 +0100

    colour css file imported to main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,21 +5,20 @@ import Contact from "../../Component/contact/Contact";
 import Footer from "../../Component/footer/Footer";
 import Services from "../../Component/service/Services";
 import Testimonials from '../../Component/testimonials/TestimonialsP';
-import styles from './globals.module.css'
+import styles from './globals.module.css';
+import './colours.css';
+
 
 
 export default function Complete() {
   return (
     <main >
       {/* <NavBar/> */}
-      {/* <h1>ACA PONDRIA TODO</h1> */}
       <Home className={styles.About}/>
-      
       <About className={styles.About}/>
       <Services/>
       <Testimonials/>
       <Contact/>
-    
       <Footer/>
     </main>
   )
