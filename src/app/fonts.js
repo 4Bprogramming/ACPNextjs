d6f8ae23ce656dd8f06f4538e4bc75e6b44e756a
neleon10 <cvn45@hotmail.com>
2024-01-03 15:30:33 +0100

    font file created

---
+++ src/app/fonts.js
@@ -0,0 +1,14 @@
+
+import localFont from 'next/font/local'
+
+export const monserrat = localFont({
+  src: './fonts/Montserrat-Italic.ttf',
+  display: 'swap',
+  variable:"--monserrat_italic"
+});
+
+export const lovelo = localFont({
+  src: './fonts/LoveloBlack.otf',
+  display: 'swap',
+  variable:"--loveloo"
+});
