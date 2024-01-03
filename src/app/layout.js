82fc7a3b85d144fe18450316e7ecca44f07065a4
neleon10 <cvn45@hotmail.com>
2024-01-03 15:30:55 +0100

    fonts included in the layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,13 @@
-import { Inter } from "next/font/google";
 
 import NavBar from "../../Component/Navbar/NavBar3.jsx";
 import "./global.css";
 import "./colours.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "ACP | Consultoría",
-  description: "Consultora Número Uno en el la región."
- 
-};
+import { monserrat, lovelo } from "./fonts.js";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>
+      <body className={`${lovelo.variable } ${monserrat.variable}`}>  
         {/* <NavBar/> */}
         {children}
       </body>
