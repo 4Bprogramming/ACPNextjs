528ef2085803655cd831941809c75503ed92f812
neleon10 <cvn45@hotmail.com>
2024-01-04 12:23:59 +0100

    testimonials uncommented

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,8 +16,8 @@ export default function Complete() {
       <Home />
       <About />
         <Services/>
-      {/*<Testimonials/>
-      <Contact/>
+      <Testimonials/>
+      {/*<Contact/>
       <Footer/> */}
     </main>
   )
