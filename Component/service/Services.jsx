aab7334337c3c1ffb8f1bef9e293b39310ab4ec4
neleon10 <cvn45@hotmail.com>
2024-01-04 11:38:06 +0100

    classes were removed

--- Component/service/Services.jsx
+++ Component/service/Services.jsx
@@ -1,27 +1,24 @@
-import styles from'./services.module.css'
-import { services } from "./service"; 
-
+import styles from "./services.module.css";
+import { services } from "./service";
+import Image from "next/image";
 
 function Services() {
-
   return (
-    <section className={styles.servicesexperience} id="services">
-      <h1 className={styles.servicesTitle}>SERVICIOS</h1>
-      <div className={styles.container}>
-        
-          {services.map((service, i) => (
-            <div key={i} className={styles.cardContainer}>
-              <div className={styles.imgSpace}>
-              <div className={styles.SvgContainer}>{service.src}</div>
-              </div>
-              <div className={styles.titleCard}>{service.title}</div>
-              <p className={styles.serviceDesciption} >{service.desc}</p>
+    <section className={`${styles.servicesSection} ${"globalWidth"}`}>
+      <h2>servicios</h2>
+      <div>
+        {services.map((service, i) => (
+          <div key={i} className={styles.serviceCard}>
+            <div>
+            {service.src}
             </div>
-          ))}
-        </div>
-  
+            <h3>{service.title}</h3>
+            <p>{service.desc}</p>
+          </div>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
 
-export default Services
+export default Services;
