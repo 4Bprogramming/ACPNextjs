a6b6f8664cbb5de5a523ca071c97cbda46e50ad3
neleon10 <cvn45@hotmail.com>
2024-01-04 14:02:47 +0100

    font added to body

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,12 @@
 import NavBar from "../../Component/Navbar/NavBar3.jsx";
 import "./global.css";
 import "./colours.css";
-import { monserrat, lovelo } from "./fonts.js";
+import { monserrat, lovelo,monserratVariable } from "./fonts.js";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={`${lovelo.variable } ${monserrat.variable}`}>  
+      <body className={`${lovelo.variable } ${monserrat.variable} ${monserratVariable.variable}`}>  
         {/* <NavBar/> */}
         {children}
       </body>
