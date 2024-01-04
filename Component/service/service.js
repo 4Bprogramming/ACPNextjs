d90e2f1b0b25c2a8ebee272316391f3716392807
neleon10 <cvn45@hotmail.com>
2024-01-04 11:37:38 +0100

    alt added to the image

--- Component/service/service.js
+++ Component/service/service.js
@@ -4,21 +4,24 @@ import inge from '../../Assets/inge.jpg'
 import Image from 'next/image'
 export const services =[
     {
-      src: <Image  src={mano} width={300} height={200}  />,
+      src: <Image  src={mano} width={300} height={200}  alt="Coach Empresarial" />,
       title: "Coach Empresarial",
       desc: "Capacitacion a equipos de trabajo y mandos medios, para lograr la profesionalización.  Aplicación de técnicas de coaching que permiten agilizar los cambios.",
       active: false,
+     
     },
     {
-      src:<Image  src={laptop} width={300} height={200}/>,
+      src:<Image  src={laptop} width={300} height={200} alt="Imagen Corporativa" />,
       title: "Imagen Corporativa",
       desc:  "Creación y desarrollo de imagen corporativa. Diseño y desarrollo web. Marketing digital: Creacion de contenidos para redes sociales. Asesoramiento Publicitario.",
       active: true,
+      
     },
     {
-      src: <Image  src={inge} width={300} height={200} />,
+      src: <Image  src={inge} width={300} height={200} alt="Gestión Interna" />,
       title: "Gestión Interna",
       desc:  "Auditoria de Gestión interna: Planificación y reorganización de los objetivos de la empresa. Gestión de procedimientos internos, personal, y agenda de clientes.",
       active: false,
+      
     },
   ];
